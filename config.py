491428f79700d6bbc1444016b5221a46faf9e8bd
eedu7 <mueedahmad067@gmail.com>
2024-10-07 09:18:05 +0500

    minor: added the config file, and load the env variables

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+
+
+class Config(BaseConfig):
+    MYSQL_URL: str | None = os.getenv('MYSQL_URL')
+    JWT_SECRET_KEY: str | None = os.getenv('JWT_SECRET_KEY')
+    JWT_ALGORITHM: str | None = os.getenv('JWT_ALGORITHM')
+
+
+config: Config = Config()
