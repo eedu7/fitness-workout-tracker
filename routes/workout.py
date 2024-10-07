10bc9f2f4941fb1ebad60d5644f2afcb103c3e6f
eedu7 <mueedahmad067@gmail.com>
2024-10-07 22:25:25 +0500

    FEAT: added the api strucutre of the workoutplan

---
+++ routes/workout.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException, status
+
+
+router: APIRouter = APIRouter()
+
+@router.post("/")
+async def create_new_workout_plan():
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail="Not Implemented",
+    )
+
+@router.get("/")
+async def get_workout_plans():
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail="Not Implemented",
+    )
+
+@router.get("/{workout_id}")
+async def get_workout_plan(workout_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail="Not Implemented",
+    )
+
+@router.put("/{workout_id}")
+async def update_workout_plan(workout_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail="Not Implemented",
+    )
+
+@router.delete("/{workout_id}")
+async def delete_workout_plan(workout_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail="Not Implemented",
+    )
