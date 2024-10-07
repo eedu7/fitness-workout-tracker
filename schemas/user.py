a23317f0d59646d4af3422cde5fb5280118771cf
eedu7 <mueedahmad067@gmail.com>
2024-10-07 09:50:57 +0500

    FEAT: added user schemas
    """
    UserResponse: with id, name and email
    
    UserRegister: with name, email and password
    
    and
    
    UserLogin: with email and password
    
    """

---
+++ schemas/user.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    name: str = Field(..., examples=["Uzumaki Naruto"])
+    email: EmailStr = Field(..., examples=["uzumaki.naruto@konoha.com"])
+
+
+class UserRegister(UserBase):
+    password: str = Field(..., examples=["Password123"])
+
+
+class UserLogin(UserBase):
+    email: EmailStr = Field(..., examples=["uzumaki.naruto@konoha.com"])
+    password: str = Field(..., examples=["Password123"])
+
+
+
+class UserResponse(BaseModel):
+    id: int = Field(..., examples=[1])
