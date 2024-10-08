fe74de7be88c81ab42047811572964f534dcdcf1
eedu7 <mueedahmad067@gmail.com>
2024-10-08 14:39:59 +0500

    MINOR: separtion of files, api and schemas

--- routes/category.py
+++ routes/category.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.category import CategoryCrud
 from db import get_async_session
+from schemas.category import CategoryCreateData, CategoryUpdateData, CategoryPartialUpdateData
 
 router: APIRouter = APIRouter()
 
 
+
 @router.get("/")
 async def get_all_categories_api(
-    skip: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)
+        skip: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     return await category_crud.get_all_categories(skip, limit)
@@ -19,40 +20,16 @@ async def get_all_categories_api(
 
 @router.get("/{category_id}")
 async def get_category_by_id_api(
-    category_id: int, session: AsyncSession = Depends(get_async_session)
+        category_id: int, session: AsyncSession = Depends(get_async_session)
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     return await category_crud.get_category_by_id(category_id)
 
 
-class CategoryCreateData(BaseModel):
-    name: str = Field(..., examples=["Endurance"])
-    description: str = Field(
-        ...,
-        examples=[
-            "Training designed to increase stamina and overall fitness, typically involving prolonged activities."
-        ],
-    )
-
-
-class CategoryUpdateData(CategoryCreateData):
-    pass
-
-
-class CategoryPartialUpdateData(BaseModel):
-    name: str | None = Field(None, examples=["Endurance"])
-    description: str | None = Field(
-        None,
-        examples=[
-            "Training designed to increase stamina and overall fitness, typically involving prolonged activities."
-        ],
-    )
-
-
 @router.post("/")
 async def create_new_category_api(
-    category_data: CategoryCreateData,
-    session: AsyncSession = Depends(get_async_session),
+        category_data: CategoryCreateData,
+        session: AsyncSession = Depends(get_async_session),
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     return await category_crud.create_category(**category_data.model_dump())
@@ -60,9 +37,9 @@ async def create_new_category_api(
 
 @router.put("/{category_id}")
 async def update_category_api(
-    category_id: int,
-    category_data: CategoryUpdateData,
-    session: AsyncSession = Depends(get_async_session),
+        category_id: int,
+        category_data: CategoryUpdateData,
+        session: AsyncSession = Depends(get_async_session),
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     return await category_crud.update_category(
@@ -72,9 +49,9 @@ async def update_category_api(
 
 @router.patch("/{category_id}")
 async def update_category_api(
-    category_id: int,
-    category_data: CategoryPartialUpdateData,
-    session: AsyncSession = Depends(get_async_session),
+        category_id: int,
+        category_data: CategoryPartialUpdateData,
+        session: AsyncSession = Depends(get_async_session),
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     return await category_crud.update_category(
@@ -84,7 +61,7 @@ async def update_category_api(
 
 @router.delete("/{category_id}")
 async def delete_category_api(
-    category_id: int, session: AsyncSession = Depends(get_async_session)
+        category_id: int, session: AsyncSession = Depends(get_async_session)
 ):
     category_crud: CategoryCrud = CategoryCrud(session)
     deleted = await category_crud.delete_category(category_id)
---
+++ schemas/category.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+class CategoryCreateData(BaseModel):
+    name: str = Field(..., examples=["Endurance"])
+    description: str = Field(
+        ...,
+        examples=[
+            "Training designed to increase stamina and overall fitness, typically involving prolonged activities."
+        ],
+    )
+
+
+class CategoryUpdateData(CategoryCreateData):
+    pass
+
+
+class CategoryPartialUpdateData(BaseModel):
+    name: str | None = Field(None, examples=["Endurance"])
+    description: str | None = Field(
+        None,
+        examples=[
+            "Training designed to increase stamina and overall fitness, typically involving prolonged activities."
+        ],
+    )
