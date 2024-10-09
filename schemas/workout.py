85a12cd9b4f09afef907032bd02c736c29c224d6
eedu7 <mueedahmad067@gmail.com>
2024-10-09 09:59:31 +0500

    MINOR: added the example, to the schema

--- schemas/workout.py
+++ schemas/workout.py
@@ -11,10 +11,10 @@ class WorkoutStatus(StrEnum):
 
 
 class WorkoutBase(BaseModel):
-    sets: int | None = Field(None, description="Number of sets")
-    repetitions: int | None = Field(None, description="Number of repetitions")
-    weight: float | None = Field(None, description="Workout weight")
-    status: WorkoutStatus | None = Field(None, description="Workout status")
+    sets: int | None = Field(None, description="Number of sets", examples=[1])
+    repetitions: int | None = Field(None, description="Number of repetitions", examples=[1])
+    weight: float | None = Field(None, description="Workout weight", examples=[190])
+    status: WorkoutStatus | None = Field(None, description="Workout status", examples=[WorkoutStatus.TO_BE_STARTED])
 
 
 class WorkoutCreate(WorkoutBase):
