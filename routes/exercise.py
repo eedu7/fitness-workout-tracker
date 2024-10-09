b7e3cac0ce7ea470160aaf63b00c82233ee5bb85
eedu7 <mueedahmad067@gmail.com>
2024-10-09 07:31:15 +0500

    MINOR: created the get by id api and fixed the database schema

--- routes/exercise.py
+++ routes/exercise.py
@@ -1,6 +1,12 @@
+from sys import get_asyncgen_hooks
+
 from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Depends
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from crud.exercise import ExerciseCrud
+from db import get_async_session
 from dependencies.authentication import AuthenticationRequired
 from schemas.exercise import ExerciseCreate
 
@@ -8,20 +14,18 @@ router: APIRouter = APIRouter(dependencies=[Depends(AuthenticationRequired)])
 
 
 @router.get("/")
-async def get_all_exercise():
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        detail="Not Implemented",
-    )
+async def get_all_exercise(skip: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)):
+    exercise_crud: ExerciseCrud = ExerciseCrud(session)
+    return await exercise_crud.get_all_exercise(skip, limit)
 
 
 @router.get("/{exercise_id}")
-async def get_exercise(exercise_id):
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED,
-        detail="Not Implemented",
-    )
+async def get_exercise(exercise_id, session: AsyncSession = Depends(get_async_session)):
+    exercise_crud: ExerciseCrud = ExerciseCrud(session)
+    return await exercise_crud.get_by_id(exercise_id)
 
+class ExerciseData(BaseModel):
+    ...
 
 @router.post("/")
 async def create_new_exercise(exercise_data: ExerciseCreate):
--- schemas/exercise.py
+++ schemas/exercise.py
@@ -1,25 +1,6 @@
-from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel, Field
-
-
-class ExerciseCategory(str, Enum):
-    CARDIO = "Cardio"
-    STRENGTH = "Strength"
-    FLEXIBILITY = "Flexibility"
-    BALANCE = "Balance"
-    OTHER = "Other"  # You can add more categories as needed
-
-
-class MuscleGroup(str, Enum):
-    CHEST = "Chest"
-    BACK = "Back"
-    LEGS = "Legs"
-    ARMS = "Arms"
-    SHOULDERS = "Shoulders"
-    CORE = "Core"
-    OTHER = "Other"  # You can add more muscle groups as needed
 
 
 class ExerciseCreate(BaseModel):
@@ -27,20 +8,18 @@ class ExerciseCreate(BaseModel):
     description: Optional[str] = Field(
         None, description="A detailed description of the exercise."
     )
-    category: ExerciseCategory = Field(
+    category_id: int = Field(
         ...,
-        description="The category of the exercise (e.g., Cardio, Strength).",
-        examples=[
-            ExerciseCategory.CARDIO,
-            ExerciseCategory.STRENGTH,
-            ExerciseCategory.FLEXIBILITY,
-        ],
+        description="The category id of the exercise (e.g., Cardio, Strength).",
+        examples=[1, 2
+                  ],
     )
-    muscle_group: MuscleGroup = Field(
+    muscle_group_id: int = Field(
         ...,
         description="The primary muscle group targeted by the exercise.",
-        examples=[MuscleGroup.CHEST, MuscleGroup.BACK, MuscleGroup.LEGS],
+        examples=[1, 2],
     )
 
     class Config:
-        orm_mode = True  # This allows the model to work with SQLAlchemy objects
+        orm_mode = True
+
